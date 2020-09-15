1a2befeed6de8334b2f35570e6b04b70de22fbda
dmitriy-ilchenko <mnt912@gmail.com>
2020-09-15 17:28:56 +0300

    Improve continuous scanning

---
+++ app/src/main/java/com/example/barcodescanner/extension/Result.kt
@@ -0,0 +1,8 @@
+package com.example.barcodescanner.extension
+
+import com.example.barcodescanner.model.Barcode
+import com.google.zxing.Result
+
+fun Result.equalTo(barcode: Barcode?): Boolean {
+    return barcodeFormat == barcode?.format && text == barcode?.text
+}
